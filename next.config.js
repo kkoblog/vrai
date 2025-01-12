63d46c74b0d3e340bf79b4704f5c170cebd8e1e7
kkoblog <koba0721a@gmail.com>
2025-01-12 09:02:49 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: 'loose'
-  },
-  webpack: (config) => {
-    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
-    return config;
-  },
-};
+  images: {
+    unoptimized: true,
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
